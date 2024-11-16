b13ad9827c49609028ca836c2fe9dac9b1b1c3ab
shibazzaki <failikyt400@gmail.com>
2024-11-16 20:44:36 +0200

    Додав файли до репозиторію

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from .extensions import db, migrate, jwt, swagger
+from .auth.controllers import auth_bp
+from .recipes.controllers import recipes_bp
+from app.extensions import db
+from app.auth.models import User, Admin
+from app.recipes.models import Recipe, Ingredient, RecipeIngredient, FavoriteRecipe
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+
+    # Ініціалізація розширень
+    db.init_app(app)
+    migrate.init_app(app, db)
+    jwt.init_app(app)
+    swagger.init_app(app)
+
+    # Реєстрація blueprints
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+    app.register_blueprint(recipes_bp, url_prefix='/recipes')
+
+    return app
---
+++ app/recipes/models.py
@@ -0,0 +1,34 @@
+from app.extensions import db
+
+class Recipe(db.Model):
+    __tablename__ = 'recipes'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    steps = db.Column(db.Text, nullable=False)
+    time_to_cook = db.Column(db.Integer, nullable=False)  # Час у хвилинах
+    servings = db.Column(db.Integer, nullable=False)
+    ingredients = db.relationship('RecipeIngredient', back_populates='recipe', lazy=True)
+    favorites = db.relationship('FavoriteRecipe', back_populates='recipe', lazy=True)
+
+class Ingredient(db.Model):
+    __tablename__ = 'ingredients'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    recipes = db.relationship('RecipeIngredient', back_populates='ingredient', lazy=True)
+
+class RecipeIngredient(db.Model):
+    __tablename__ = 'recipe_ingredients'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'), nullable=False)
+    ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredients.id'), nullable=False)
+    recipe = db.relationship('Recipe', back_populates='ingredients')
+    ingredient = db.relationship('Ingredient', back_populates='recipes')
+
+class FavoriteRecipe(db.Model):
+    __tablename__ = 'favorite_recipes'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'), nullable=False)
+    user = db.relationship('User', back_populates='favorites')
+    recipe = db.relationship('Recipe', back_populates='favorites')
